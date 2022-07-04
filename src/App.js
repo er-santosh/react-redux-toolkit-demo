45c32ef68d70982e323ae881d2287a568b38ee11
er-santosh <er.santoshtharu@gmail.com>
2022-07-04 13:33:15 +0545

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import { Fragment } from "react";
-import Auth from "./components/pages/Auth";
-import Counter from "./components/pages/Counter";
+import { Fragment, Suspense, lazy } from "react";
 import Header from "./components/layout/Header";
-import UserProfile from "./components/pages/UserProfile";
 import { Route, Routes, Navigate } from "react-router-dom";
-
+import { ScaleLoader } from "react-spinners";
 function App() {
+  const Auth = lazy(() => import("./components/pages/Auth"));
+  const UserProfile = lazy(() => import("./components/pages/UserProfile"));
+  const Counter = lazy(() => import("./components/pages/Counter"));
+  const loaderConfig = {
+    color: "#FFFFFF",
+  };
+
+  const override = {
+    display: "block",
+    marginLeft: "50%",
+    marginTop: "30%",
+  };
   return (
     <Fragment>
       <Header />
-      <Routes>
-        <Route path="/" element={<Navigate to="/counter" />}></Route>
-        <Route path="/login" element={<Auth />}></Route>
-        <Route path="/profile" element={<UserProfile />}></Route>
-        <Route path="/counter" element={<Counter />}></Route>
-      </Routes>
+      <Suspense
+        fallback={
+          <ScaleLoader color={loaderConfig.color} cssOverride={override} />
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Navigate to="/counter" />}></Route>
+          <Route path="/login" element={<Auth />}></Route>
+          <Route path="/profile" element={<UserProfile />}></Route>
+          <Route path="/counter" element={<Counter />}></Route>
+        </Routes>
+      </Suspense>
     </Fragment>
   );
 }
